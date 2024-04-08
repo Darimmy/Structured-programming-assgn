18f009eea80526335693cdba3676dd39e758b1c6
Rogers JIm <144924036+Darimmy@users.noreply.github.com>
2024-04-08 23:26:00 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,2 +1,12 @@
-void populate_vehicle();
-void print_vehicle();
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+struct Cars {
+    char brand[50];
+    char model[50];
+};
+
+void populateCar(struct Cars[]);
+void printCar(struct Cars[]);
+
+#endif
