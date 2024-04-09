80d0aade1f395507223d4caf8d7fe2bc3c779ccc
Rogers JIm <144924036+Darimmy@users.noreply.github.com>
2024-04-09 14:48:27 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,18 +2,22 @@
 #include "functions.h"
 
 void populateCar(struct Cars car[10]) {
-    for (int i = 0; i < 10; i++) {
-        printf("Enter the Brand of the vehicle %d: ", i+1);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Enter the Brand of vehicle %d: ", i+1);
         scanf("%s", car[i].brand);
 
-        printf("Enter the model of the vehicle %d: ", i+1);
+        printf("Enter the model of vehicle %d: ", i+1);
         scanf("%s", car[i].model);
+        
+        printf("Enter the year of production of vehicle %d: ", i+1);
+        scanf("%d", car[i].year);
     }
 }
 
 void printCar(struct Cars car[10]) {
     printf("\nAvailable cars include:\n");
     for (int i = 0; i < 10; i++) {
-        printf("Cars %d: %s by %s\n", i+1, car[i].brand, car[i].model);
+        printf("Cars %d: %s by %s %d\n", i+1, car[i].model, car[i].brand, car[i].year);
     }
 }
